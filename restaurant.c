d83882f2a638fc3654acfb23dc5bcdc5ffb9d517
Takyu Fung <takyufung@cpp.edu>
2021-09-24 14:17:39 -0700

    Added restaurant code to Github

---
+++ restaurant.c
@@ -0,0 +1,9 @@
+// This program will compute the tax and tip on a restauront for a customer
+// Will have switch cases for the type of meal and would display the cost, tax, tips, and total.
+
+#include <stdio.h>
+
+int main()
+{
+    printf("test");
+}
